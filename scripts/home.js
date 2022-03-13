657471b646ac76306fd27af4cca74b74bdc1a46c
MDLauriers36 <90851590+MDLauriers36@users.noreply.github.com>
2022-03-12 22:44:36 -0500

    linking

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,4 @@
-import Swiper from 'swiper';
-  // import Swiper styles
-  import 'swiper/css';
-  let swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  let swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
   });
