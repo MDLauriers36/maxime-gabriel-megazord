0e146f8d612dd2726bad89a1b175de2d33c7d524
Snxww <90851816+Snxww@users.noreply.github.com>
2022-03-30 18:41:10 -0400

    added all animation points + changed classes again for srollTrigger control

--- scripts/discography.js
+++ scripts/discography.js
@@ -5,3 +5,50 @@ let swiper = new Swiper(".swiper", {
 });
 
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.timeline()
+    .to('.no2', {
+        scrollTrigger: {
+            markers: true,
+            start: 'center 75%',
+            trigger: '.no2',
+        },
+        x: '100%',
+        duration: 2,
+    })
+    .to('.no2', {
+        scrollTrigger: {
+            markers: true,
+            start: 'center 75%',
+            trigger: '.no2',
+        },
+        x: '100%',
+        duration: 2,
+    })
+    .to('.no2', {
+        scrollTrigger: {
+            markers: true,
+            start: 'center 75%',
+            trigger: '.no2',
+        },
+        x: '100%',
+        duration: 2,
+    })
+    .to('.no2', {
+        scrollTrigger: {
+            markers: true,
+            start: 'center 75%',
+            trigger: '.no2',
+        },
+        x: '100%',
+        duration: 2,
+    })
+    .to('.no2', {
+        scrollTrigger: {
+            markers: true,
+            start: 'center 75%',
+            trigger: '.no2',
+        },
+        x: '100%',
+        duration: 2,
+    })
