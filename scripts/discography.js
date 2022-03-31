5daf2db2419150836d21495330ad5cf243c34edd
Snxww <90851816+Snxww@users.noreply.github.com>
2022-03-30 21:39:49 -0400

    multiple attemps

--- scripts/discography.js
+++ scripts/discography.js
@@ -6,15 +6,65 @@ let swiper = new Swiper(".swiper", {
 
 gsap.registerPlugin(ScrollTrigger);
 
-    gsap.timeline({
-        scrollTrigger: {
-          markers: true,
-          start: 'top 75%',
-          end: 'bottom 10%',
-          toggleActions: 'restart reset play',
-          trigger: 'section',
-        },
-      })
-        .from('.iridescent', { x: '110%' })
-        .from('.iridescentDetails', { x: '-155%' }, '<' )
+    // gsap.timeline({
+    //     scrollTrigger: {
+    //       markers: true,
+    //       start: 'top 50%',
+    //       end: 'bottom 10%',
+    //       toggleActions: 'restart reset play reverse',
+    //       trigger: 'section',
+    //     },
+    //   })
+    //     .from('.iridescent', { x: '110%' })
+    //     .from('.iridescentDetails', { x: '-155%' }, '<' )
+    //     .from('.wteb', { x: '-165%' })
+    //     .from('.wtebDetails', { x: '110%' });
         
+        // gsap.timeline()
+        // .from('.iridescent', { x: '110%', scrollTrigger: {
+        //     markers: true,
+        //     start: 'top 50%',
+        //     end: 'bottom 10%',
+        //     toggleActions: 'play',
+        //     trigger: '.album-iridescent',
+        //   }, })
+        // .from('.iridescentDetails', { x: '-155%', scrollTrigger: {
+        //     markers: true,
+        //     start: 'top 50%',
+        //     end: 'bottom 10%',
+        //     toggleActions: 'restart reset play reverse',
+        //     trigger: '.album-iridescent',
+        //   }, }, '<' )
+        // .from('.wteb', { x: '-165%', scrollTrigger: {
+        //     markers: true,
+        //     start: 'top 50%',
+        //     end: 'bottom 10%',
+        //     toggleActions: 'restart reset play reverse',
+        //     trigger: '.album-wteb',
+        //   }, })
+        // .from('.wtebDetails', { x: '110%', scrollTrigger: {
+        //     markers: true,
+        //     start: 'top 50%',
+        //     end: 'bottom 10%',
+        //     toggleActions: 'restart reset play reverse',
+        //     trigger: '.album-wteb',
+        //   }, });
+
+    // let sections = gsap.utils.toArray('.album');
+
+    // sections.forEach((section) => {
+      
+    //   gsap.timeline(section, {
+    //     scrollTrigger: {
+    //         trigger: section,
+    //         start: 'top 50%',
+    //         end: 'bottom 10%',
+    //         toggleActions: 'restart reset play reverse',
+    //         markers: true
+    //     },
+    // })
+    // .from('.iridescent', { x: '110%' })
+    // .from('.iridescentDetails', { x: '-155%' }, '<' )
+    // .from('.wteb', { x: '-165%' })
+    // .from('.wtebDetails', { x: '110%' })
+    // });
