804c8a3721e06c48e254238d64ce5abb0b9ea4d4
Snxww <90851816+Snxww@users.noreply.github.com>
2022-04-13 19:30:13 -0400

    fixed indentation

--- scripts/discography.js
+++ scripts/discography.js
@@ -6,44 +6,44 @@ let swiper = new Swiper(".swiper", {
 
 gsap.registerPlugin(ScrollTrigger);
 
-    let sections = document.querySelectorAll('section');
-    sections.forEach( function(section) {
-        let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: section,
-                start: 'top 50%',
-                end: 'bottom 10%',
-                toggleActions: 'restart reset play reverse'
-            }
-        });
-        tl.fromTo(section,
-            { opacity: '0%'},
-            { opacity: '100%'}
-        );
+let sections = document.querySelectorAll('section');
+sections.forEach( function(section) {
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: section,
+            start: 'top 50%',
+            end: 'bottom 10%',
+            toggleActions: 'restart reset play reverse'
+        }
     });
+    tl.fromTo(section,
+        { opacity: '0%'},
+        { opacity: '100%'}
+    );
+});
 
-    let detailsDroite = document.querySelectorAll('.detailsDroite');
-    detailsDroite.forEach( function(detailDroit) {
-        let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: detailDroit,
-                start: 'top 50%',
-                end: 'bottom 10%',
-                toggleActions: 'restart reset play reverse'
-            }
-        });
-        tl.from(detailDroit, { x: '110%' });
+let detailsDroite = document.querySelectorAll('.detailsDroite');
+detailsDroite.forEach( function(detailDroit) {
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: detailDroit,
+            start: 'top 50%',
+            end: 'bottom 10%',
+            toggleActions: 'restart reset play reverse'
+        }
     });
+    tl.from(detailDroit, { x: '110%' });
+});
 
-    let detailsGauche = document.querySelectorAll('.detailsGauche');
-    detailsGauche.forEach( function(detailGauche) {
-        let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: detailGauche,
-                start: 'top 50%',
-                end: 'bottom 10%',
-                toggleActions: 'restart reset play reverse'
-            }
-        });
-        tl.from(detailGauche, { x: '-165%' });
+let detailsGauche = document.querySelectorAll('.detailsGauche');
+detailsGauche.forEach( function(detailGauche) {
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: detailGauche,
+            start: 'top 50%',
+            end: 'bottom 10%',
+            toggleActions: 'restart reset play reverse'
+        }
     });
+    tl.from(detailGauche, { x: '-165%' });
+});
